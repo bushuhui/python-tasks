6fed62d2a0b7b5510ec1c9fae77a129d5bf1a235
npu-chenlin <npu-chenlin@users.noreply.github.com>
2018-06-11 14:33:49 +0800

    13

--- 0013/find_imgs.py
+++ 0013/find_imgs.py
@@ -1,23 +1,19 @@
-from bs4 import BeautifulSoup as bs
-import requests
-
-def find_links(html):
-	links=[]
-	c=bs(html,'lxml')
-	l=c.find_all('a')
-	root='https://github.com'
-	for link in l:
-		if link['href'] and link['href'][0]=='/':
-			links.append(root+link['href'])
-		else:
-			links.append(link['href'])
+import requests,re
+from PIL import Image
+def find_img_links(html):
+	pat=re.compile('src="(.+?.jpg)" bdwater=')
+	links=re.findall(pat,html)
 	return links
 
-
 if __name__=='__main__':
-	html=requests.get('https://github.com/Yixiaohan/show-me-the-code')
+	html=requests.get('http://tieba.baidu.com/p/2166231880')
 	html=html.text
-	content=find_links(html)
-	with open('links.txt','w',encoding='utf-8') as f:
-		f.write('\n'.join(content))
+	links=find_img_links(html)
+	print(links)
+	count=0
+	for i in links:
+		img=requests.get(i)
+		with open(str(count)+'.jpg','wb') as f:
+			f.write(img.content)
+		count+=1
 
